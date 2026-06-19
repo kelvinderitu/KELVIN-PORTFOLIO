f33a5eea9510283dad6925e7170d05503b23c242
kelvin nderitu <134921349+kelvinderitu@users.noreply.github.com>
2026-06-19 12:46:51 +0300

    remove invalid github token

--- src/services/github.ts
+++ src/services/github.ts
@@ -4,14 +4,7 @@ const USERNAME = "kelvinderitu";
 
 export const getProjects = async () => {
   const { data } = await axios.get(
-    `https://api.github.com/users/${USERNAME}/repos?sort=updated`,
-    {
-      headers: {
-        Authorization: `Bearer ${
-          import.meta.env.VITE_GITHUB_TOKEN
-        }`,
-      },
-    }
+    `https://api.github.com/users/${USERNAME}/repos?sort=updated`
   );
 
   return data.filter(
