2ca6a85a8f2bb55721efa38b070098a4184fff23
kelvin nderitu <134921349+kelvinderitu@users.noreply.github.com>
2026-06-19 12:17:35 +0300

    prj_xlBSZ3A9hxmx37nGZNhySI7Lkcey

--- src/services/github.ts
+++ src/services/github.ts
@@ -4,11 +4,17 @@ const USERNAME = "kelvinderitu";
 
 export const getProjects = async () => {
   const { data } = await axios.get(
-    `https://api.github.com/users/${USERNAME}/repos?sort=updated`
+    `https://api.github.com/users/${USERNAME}/repos?sort=updated`,
+    {
+      headers: {
+        Authorization: `Bearer ${
+          import.meta.env.VITE_GITHUB_TOKEN
+        }`,
+      },
+    }
   );
 
   return data.filter(
-    (repo: any) =>
-      !repo.fork
+    (repo: any) => !repo.fork
   );
 };
